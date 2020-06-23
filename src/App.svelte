776436afb7980886ffb40b7bdff7644d1313cf7f
gawema <gab.mannu@hotmail.it>
2020-06-24 01:03:52 +0200

    added login & signup using tailwindui.

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,14 @@
   import { Router, Route, Link } from "svelte-routing";
   import Home from "./pages/Home.svelte";
   import Login from "./pages/Login.svelte";
+  import Signup from "./pages/Signup.svelte";
   export let url = ""; //This property is necessary declare to avoid ignore the Router
 </script>
 
 <Router url="{url}">
- <nav>
-    <Link to="/">Home</Link>
-    <Link to="/login">About</Link>
-  </nav>
   <div>
     <Route path="/login" component="{Login}" /> 
-    <!--for now the router just support case sensitive,
-        one workaround colud be add two time the route
-        Example.
-       <Route path="About" component="{About}" /> 
-    -->
+    <Route path="/signup" component="{Signup}" /> 
     <Route path="/"><Home /></Route>
   </div>
 </Router>
