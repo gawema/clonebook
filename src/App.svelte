e93d27c558461204b8e82f9b100e3044060de3b6
gawema <gab.mannu@hotmail.it>
2020-06-23 23:03:37 +0200

    added routing with svelte-routing.

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,22 @@
 <script>
-	import Nav from './sections/Nav.svelte'
-	import Timeline from './sections/Timeline.svelte'
-	import Left from './sections/Left.svelte'
-	import Right from './sections/Right.svelte'
-	import { onMount } from "svelte"
-	import {me} from "./store.js"
-	import {post_store} from "./store.js"
-	import {getPosts} from "./data.js"
-
-
-	let posts = [];
-	onMount(() => {
-		storePosts();
-	});
-
-	async function storePosts() {
-		$post_store = await getPosts();
-	}
-	
-	post_store.subscribe(data => {
-		posts = data;
-	})
-
+  import { Router, Route, Link } from "svelte-routing";
+  import Home from "./pages/Home.svelte";
+  import Login from "./pages/Login.svelte";
+  export let url = ""; //This property is necessary declare to avoid ignore the Router
 </script>
 
-<!-- ########################### -->
-
-<main>
-	<Nav />
-	<div>{$me.name} {$me.lastName}</div>
-	<div>
-		<Left />
-	{#if posts}
-		<Timeline posts={posts}/>
-	{/if}
-		<Right />
-	</div>
-
-</main>
-
-<!-- ########################### -->
-
-<style>
-	div{
-		display: grid;
-		grid-template-columns: 10fr 20fr 10fr;
-		grid-gap: 5rem;
-		position: absolute;
-		top: 5rem;
-		left: 0px;
-		width: 100%;
-		height: 100%;
-		padding: 0px 2vw;
-	}
-</style>
+<Router url="{url}">
+ <nav>
+    <Link to="/">Home</Link>
+    <Link to="/login">About</Link>
+  </nav>
+  <div>
+    <Route path="/login" component="{Login}" /> 
+    <!--for now the router just support case sensitive,
+        one workaround colud be add two time the route
+        Example.
+       <Route path="About" component="{About}" /> 
+    -->
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,56 @@
+<script>
+	import Nav from '../sections/Nav.svelte'
+	import Timeline from '../sections/Timeline.svelte'
+	import Left from '../sections/Left.svelte'
+	import Right from '../sections/Right.svelte'
+	import { onMount } from "svelte"
+	import {me} from "../store.js"
+	import {post_store} from "../store.js"
+	import {getPosts} from "../data.js"
+
+
+	let posts = [];
+	onMount(() => {
+		storePosts();
+	});
+
+	async function storePosts() {
+		$post_store = await getPosts();
+	}
+	
+	post_store.subscribe(data => {
+		posts = data;
+	})
+
+</script>
+
+<!-- ########################### -->
+
+<main>
+	<Nav />
+	<div>{$me.name} {$me.lastName}</div>
+	<div>
+		<Left />
+	{#if posts}
+		<Timeline posts={posts}/>
+	{/if}
+		<Right />
+	</div>
+
+</main>
+
+<!-- ########################### -->
+
+<style>
+	div{
+		display: grid;
+		grid-template-columns: 10fr 20fr 10fr;
+		grid-gap: 5rem;
+		position: absolute;
+		top: 5rem;
+		left: 0px;
+		width: 100%;
+		height: 100%;
+		padding: 0px 2vw;
+	}
+</style>
